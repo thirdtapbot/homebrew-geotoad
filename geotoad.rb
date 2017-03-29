15d5012f1371b78634f270039c76c6e28d29a64a
Vítor Galvão <info@vitorgalvao.com>
2017-03-29 14:50:35 +0100

    Update geotoad.rb

--- geotoad.rb
+++ geotoad.rb
@@ -1,8 +1,8 @@
 class Geotoad < Formula
   desc "Query tool to query the geocaching.com website"
   homepage "https://github.com/steve8x8/geotoad"
-  url "https://github.com/steve8x8/geotoad/archive/3.26.2.tar.gz"
-  sha256 "d5fda74f4d6d9cc0bb849c5474447e1b65e7cd2cdae0682f71d807581ad8a6b3"
+  url "https://github.com/steve8x8/geotoad/archive/3.27.0.tar.gz"
+  sha256 "2a851e1938d7a1e713cfa4720dcbf2ceaf0266f190a62c728b6def8aea2dcf5b"
   head "https://github.com/steve8x8/geotoad.git"
 
   def install
